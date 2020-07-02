f00a3ada5d92d3033ce5028fbfdbc95823251960
Luca <lucat1@users.noreply.github.com>
2020-07-02 14:43:37 +0200

    WIP(feat): attempt time formatting, still broken

--- modules/duration.c
+++ modules/duration.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,16 +9,34 @@
 #define TIME_MINUTE TIME_SECOND * 60
 #define TIME_HOUR TIME_MINUTE * 60
 
-
 void duration_enter() {
-  int prompt_duration;
-  /* char *fmt; */
-  prompt_duration = get_env_int("PROMPT_DURATION");
+  int dur;
+  char *fmt;
+  dur = get_env_int("PROMPT_DURATION");
 
   // the duration is in milliseconds. So we apply a threshold
   // before outputting its value
-  if (prompt_duration > DURATION_SEGMENT_THRESHOLD)
-    printf("took " DURATION_SEGMENT_COLOR "%ds" RESET, prompt_duration/TIME_SECOND);
+  if (dur < DURATION_SEGMENT_THRESHOLD)
+    return;
+
+  if(dur < TIME_MINUTE)
+    // print seconds 
+    asprintf(&fmt, "%ds", dur / TIME_SECOND);
+
+  if(dur < TIME_HOUR && dur > TIME_MINUTE) {
+    // print minutes 
+    int secs = dur % TIME_MINUTE;
+    asprintf(&fmt, "%dm %ds", dur / TIME_MINUTE, secs / TIME_SECOND);
+  }
+
+  if(dur >= TIME_HOUR && dur > TIME_MINUTE) {
+    // print hours 
+    int mins = dur % TIME_HOUR;
+    asprintf(&fmt, "%dh %dm", dur / TIME_HOUR, mins / TIME_MINUTE);
+  }
+
+  printf("took " DURATION_SEGMENT_COLOR "%s" RESET, fmt);
+  free(fmt);
 }
 
 void duration_leave() {}
